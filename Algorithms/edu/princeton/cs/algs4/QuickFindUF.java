ee1a4b0d5488f33471b30ef7be1b342f67da7d9a
trthngoc <25020295@vnu.edu.vn>
2026-09-12 20:24:04 +0700

    [Week 1] ex update

--- Algorithms/edu/princeton/cs/algs4/QuickFindUF.java
+++ Algorithms/edu/princeton/cs/algs4/QuickFindUF.java
@@ -7,23 +7,23 @@ public class QuickFindUF {
         }
     }
     public void union(int p, int q) {
-        int pid = id[p];
-        int qid = id[q];
+        // int pid = id[p];
+        // int qid = id[q];
         for (int i = 0; i < id.length; i++) {
-            if (id[i] == pid) id[i] = qid;
+            if (id[i] == id[p]) id[i] = id[q];
         }
     }
     public int find(int p) {
         return id[p];
     }
     public boolean connected(int p, int q) {
-        return id[p] == id[q];
+        return find(p) == find(q);
     }
     public static void main(String[] args) {
         QuickFindUF uf = new QuickFindUF(5);
         uf.union(0, 1);
-        uf.union(1, 2);
+        uf.union(0, 2);
         System.out.println(uf.connected(0, 2)); // true
-        System.out.println(uf.connected(0, 3)); // false
+        System.out.println(uf.connected(1, 2)); // false
     }
 }
